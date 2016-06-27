16c74150a8befd1c193a3442a817d635196266c2
Iwaco <iwaco@iwaco.pink>
2016-06-27 14:03:27 +0900

    Add cookie based agent

--- lib/shja/agent.rb
+++ lib/shja/agent.rb
@@ -10,8 +10,6 @@ require 'webrick/cookie'
 require 'uri'
 
 class Shja::Agent
-  extend Memoist
-
   attr_reader :agent
   attr_reader :username
   attr_reader :password
@@ -24,10 +22,33 @@ class Shja::Agent
     @context = context
     @context.agent = self
 
+    self.is_login = false
+  end
+
+  def login
+    raise "Unimplemented"
+  end
+
+  def referrer_url
+    raise "Unimplemented"
+  end
+
+  def fetch_page(url)
+    raise "Unimplemented"
+  end
+
+  def download(url, path)
+    raise "Unimplemented"
+  end
+end
+
+class Shja::MechanizeAgent < Shja::Agent
+  extend Memoist
+
+  def initialize(username: username, password: password, context: context)
+    super
     @agent = Mechanize.new
     @agent.user_agent = 'Mac Safari'
-
-    self.is_login = false
   end
 
   def login
@@ -57,64 +78,18 @@ class Shja::Agent
   end
 end
 
-# Monkey patch for capybara-webkit
-require 'capybara/webkit/cookie_jar'
-class Capybara::Webkit::CookieJar
-
-  def cookies_for(url)
-    uri = URI.parse(url)
-    return cookies.select {|c| valid_domain?(c, uri.host)}.map {|c| "#{c.name}=#{c.value}" }.join('; ')
-  end
-end
-
-Capybara.run_server     = false
-Capybara.default_driver = :webkit
-Capybara::Webkit.configure do |config|
-  config.allow_url("d2pass.com")
-  config.allow_url("caribbeancom.com")
-  config.allow_url("1pondo.tv")
-  config.allow_url("google-analytics.com")
-  config.allow_url("googleapis.com")
-  config.allow_url("amazonaws.com")
-  config.allow_url("doubleclick.net")
-  config.allow_url("crazyegg.com")
-end
-
-class Shja::CapybaraAgent
-  include Capybara::DSL
+class Shja::CookieBasedAgent < Shja::Agent
+  attr_reader :cookies
   extend Memoist
 
-  attr_reader :agent
-  attr_reader :username
-  attr_reader :password
-  attr_reader :context
-  attr_accessor :is_login
-
-  def initialize(username: username, password: password, context: context)
-    @username = username
-    @password = password
-    @context = context
-    @context.agent = self
-    @is_login = false
-
-    init_agent
+  def initialize(username: nil, password: nil, context: nil, cookies: '')
+    super(username: username, password: password, context: context)
+    @cookies = cookies
+    self.is_login = true
   end
 
-  def init_agent
-    h = Headless.new()
-    h.start
-    @agent = page
-    ObjectSpace.define_finalizer(@agent) do
-      h.destroy
-    end
-  end
-
-  def login
-    raise "Unimplemented"
-  end
-
-  def referrer_url
-    raise "Unimplemented"
+  def cookies_for(url)
+    self.cookies
   end
 
   def create_curl_agent(url)
@@ -122,7 +97,7 @@ class Shja::CapybaraAgent
     curl_agent = Curl::Easy.new(url)
     curl_agent.follow_location = true
     curl_agent.max_redirects = 5
-    cookies = self.agent.driver.cookies.cookies_for(url)
+    cookies = self.cookies_for(url)
     Shja.log.debug("Cookies: #{cookies}")
     curl_agent.headers["Cookie"] = cookies
     return curl_agent
@@ -140,17 +115,6 @@ class Shja::CapybaraAgent
       end
     end
     return curl.body_str
-  end
-
-  def logdir
-    File.join(context.target_dir, 'logs')
-  end
-
-  def screenshot(filename)
-    if Shja.log.debug?
-      FileUtils.mkdir_p(logdir) unless File.directory?(logdir)
-      agent.save_screenshot(File.join(logdir, filename), full: true)
-    end
   end
 
   def fetch_page(url)
@@ -176,6 +140,72 @@ class Shja::CapybaraAgent
     FileUtils.rm(path) if File.file?(path)
     raise ex
   end
+end
+
+# Monkey patch for capybara-webkit
+require 'capybara/webkit/cookie_jar'
+class Capybara::Webkit::CookieJar
+
+  def cookies_for(url)
+    uri = URI.parse(url)
+    return cookies.select {|c| valid_domain?(c, uri.host)}.map {|c| "#{c.name}=#{c.value}" }.join('; ')
+  end
+end
+
+Capybara.run_server     = false
+Capybara.default_driver = :webkit
+Capybara::Webkit.configure do |config|
+  config.allow_url("d2pass.com")
+  config.allow_url("caribbeancom.com")
+  config.allow_url("1pondo.tv")
+  config.allow_url("google-analytics.com")
+  config.allow_url("googleapis.com")
+  config.allow_url("amazonaws.com")
+  config.allow_url("doubleclick.net")
+  config.allow_url("crazyegg.com")
+end
+
+class Shja::CapybaraAgent < Shja::CookieBasedAgent
+  include Capybara::DSL
+
+  def initialize(username: nil, password: nil, context: nil, cookies: '')
+    super
+    self.is_login = false
+    init_agent
+  end
+
+  def init_agent
+    h = Headless.new()
+    h.start
+    @agent = page
+    ObjectSpace.define_finalizer(@agent) do
+      h.destroy
+    end
+  end
+
+  def cookies_for(url)
+    self.agent.driver.cookies.cookies_for(url)
+  end
+
+  def login
+    raise "Unimplemented"
+  end
+
+  def referrer_url
+    raise "Unimplemented"
+  end
+
+  def logdir
+    File.join(context.target_dir, 'logs')
+  end
+
+  def screenshot(filename)
+    if Shja.log.debug?
+      FileUtils.mkdir_p(logdir) unless File.directory?(logdir)
+      agent.save_screenshot(File.join(logdir, filename), full: true)
+    end
+  end
+
 end
 
 require 'shja/agent/hc'
