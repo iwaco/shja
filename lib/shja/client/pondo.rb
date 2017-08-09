3aa3e2aaa9b9e8281437c3c353d27277e72334b3
Iwaco <iwaco@iwaco.pink>
2017-08-09 17:13:19 +0900

    Implements refresh_cache for Pondo

--- lib/shja/client/pondo.rb
+++ lib/shja/client/pondo.rb
@@ -17,6 +17,12 @@ class Shja::Client::Pondo < Shja::D2PassClient
     Shja::Html::Pondo
   end
 
+  def refresh_cache!
+    movies.all do |movie|
+      moview.download_metadata
+    end
+  end
+
   def refresh_by_actor_id!(actor_id)
     movies.download_actor_index(actor_id)
   end
