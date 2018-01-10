57e8bd3c2147a10e7d47370de0e9c7ee0b356d48
Iwaco <iwaco@iwaco.pink>
2018-01-10 18:02:57 +0900

    Fix to use random string for test

--- test/test_functional_helper.rb
+++ test/test_functional_helper.rb
@@ -21,7 +21,7 @@ class CaribFunctionalDbTest < Minitest::Test
   attr_reader :db
 
   def setup
-    @db = Shja::Db::Carib.new(endpoints: ENV['CARIB_ETCD_ENDPOINTS'])
+    @db = Shja::Db::Carib.new(prefix: "test-#{rand(10000)}", endpoints: ENV['CARIB_ETCD_ENDPOINTS'])
     db_fixture_path = File.join(FIXTURES_ROOT, 'carib', 'db.yml')
     open(db_fixture_path) do |io|
       YAML.load(io.read).each do |movie|
